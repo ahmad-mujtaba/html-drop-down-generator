43f8e109c899ee691ead0b3342d8c23a9eb81dd8
Mujtaba Ahmad <mujtaba.ahmad@knorex.com>
2018-11-30 12:36:27 +0530

    gzip response

--- files/css/css.php
+++ files/css/css.php
@@ -1,6 +1,7 @@
 <?php
 
-
+//ini_set('zlib.output_compression', 1);
+ob_start("ob_gzhandler");
 header('Content-type: text/css');
 header('ETag: "' . md5("style.css") . '"');
 header('Cache-Control: max-age=3600000, must-revalidate');
@@ -8,6 +9,8 @@ header('Cache-Control: max-age=3600000, must-revalidate');
 
 
 echo file_get_contents("prism.css").file_get_contents("style.css");
+
+ob_end_flush();
 
 
 
--- files/js/js.php
+++ files/js/js.php
@@ -1,5 +1,7 @@
 <?php
 
+//ini_set('zlib.output_compression', 1);
+ob_start("ob_gzhandler");
 header('Content-type: application/javascript');
 header('ETag: "' . md5("app.js") . '"');
 header('Cache-Control: max-age=3600000, must-revalidate');
@@ -7,6 +9,8 @@ header('Cache-Control: max-age=3600000, must-revalidate');
 echo file_get_contents("jquery-3.3.1.min.js").
 file_get_contents("prism.js").
 file_get_contents("app.js");
+
+ob_end_flush();
 
 
 
